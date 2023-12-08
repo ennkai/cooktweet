6e3aa814d9254c725a30e580acc5d4e2318a1c99
ennkai <ennkaiponn@outlook.jp>
2023-12-08 11:20:59 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :nickname, presence: true, length: { maximum: 6 }
 
   has_many :resipes
   #has_many :likes
